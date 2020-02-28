5a68526ed054e5c00671e2985e835b084e66e5bd
dad <60132399+dad2vad@users.noreply.github.com>
2020-02-29 00:46:07 +0200

    Update todos-read-all.js

--- functions/todos-read-all.js
+++ functions/todos-read-all.js
@@ -3,7 +3,7 @@ const faunadb = require('faunadb')
 
 const q = faunadb.query
 const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SERVER_SECRET
+  secret: "fnADluvTONACAr8_LnhzOyiGDEOr7bgRP4EVXkbO"
 })
 
 exports.handler = (event, context) => {
